762aad7aaa43f72c1661b367fd9e816afd8b4dbd
vikash <vikash.kumar@gorapid.io>
2019-12-26 17:56:23 +0530

    A new Home.HTML is added   *ListView is added in views.py

--- new_mb/new_mb/urls.py
+++ new_mb/new_mb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from testapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.Home_view.as_view()),
+    
 ]
--- new_mb/testapp/views.py
+++ new_mb/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from testapp.models import Post
 # Create your views here.
+class Home_view(ListView):
+    model=Post
+    template_name="testapp/home.html"
+    context_object_name="Post_list"
